d5a1601e40b295586e501a8a92d99bdc5c409100
Leo Cao <leocy29@gmail.com>
2023-09-24 00:15:42 -0700

    add css for Cart Order Menu Page

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
 import Username from '../features/user/Username';
+import LinkButton from './LinkButton';
 
 function Header() {
   return (
     <header
-      className="flex items-center justify-between border-b border-stone-500
-     bg-yellow-500 px-4 py-3 
-     uppercase sm:px-6"
+      className=" flex items-center justify-between border-b
+     border-stone-500 bg-yellow-500 px-4 
+     py-3 uppercase sm:px-6"
     >
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
