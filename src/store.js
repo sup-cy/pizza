8c81ac348b350fbb271511ffa3dcdff3988c2ee4
Leo Cao <leocy29@gmail.com>
2023-09-24 22:20:10 -0700

    create cartslice file, apply feature add and delete pizza and CartOverview Menutime order page

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './features/user/userSlice';
+import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 const store = configureStore({
   reducer: {
-    user: userSlice,
+    user: userReducer,
+    cart: cartReducer,
   },
 });
 
